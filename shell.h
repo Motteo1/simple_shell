0125f62e6b949004d6b2e05edb08c52579937a38
Benterf123 <faithbenter@gmail.com>
2022-08-10 10:33:25 -0700

    shell

--- shell.h
+++ shell.h
@@ -37,13 +37,18 @@ char *find_path(char **env);
 void check_for_path(vars_t *vars);
 int execute_cwd(vars_t *vars);
 int check_for_dir(char *str);
-=======
 char *_which(char *str, list_t *env);
 void free_double_ptr(char **str);
 void free_linked_list(list_t *list);
 void not_found(char *str, int num, list_t *env);
 int main(int ac, char **av, char **env);
 int prompt(char **env);
+int path_execute(char *command, vars_t *vars);
+char *find_path(char **env);
+void check_for_path(vars_t *vars);
+int execute_cwd(vars_t *vars);
+int check_for_dir(char *str);
 >>>>>>> ede80d76a0a504f982d2db512d189d61e6e25c0c
+
 
 #endif
