12fa49691f4485804b38c4f0239d30120d6e9d0e
Motteo1 <timothykirungu@gmail.com>
2022-08-10 16:22:43 -0700

    Concatenate cd error

---
+++ aux_error1.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * strcat_cd - function that concatenates the message for cd error
+ *
+ * datash: data relevant (directory)
+ * @msg: message to print
+ * @error: output message
+ * @ver_str: counter lines
+ * Return: error message
+ */
